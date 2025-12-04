bd9ad9a3418d974c465e9f11b66c3e6873127c25
Dana <dkbuchshtav@gmail.com>
2025-12-04 07:55:59 +0100

    basic apis

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,28 @@
-from fastapi import FastAPI
+from typing import Annotated
+from fastapi import FastAPI, Depends
 
 app = FastAPI()
 
+class DiagramStorage:
+    def __init__(self):
+        self.diagram = None
+    
+    def get_diagram(self):
+        return self.diagram
+    
+    def save_diagram(self, new_diagram: str):
+        self.diagram = new_diagram
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+
+async def diagram(d: DiagramStorage):
+    return d
+
+
+@app.get("/get-diagram")
+async def get_diagram(d: Annotated[DiagramStorage, Depends(diagram)]):
+    return {"diagram": d.get_diagram()}
+
+@app.post("/save-diagram")
+async def save_diagram(new_diagram: str, d: Annotated[DiagramStorage, Depends(diagram)]):
+    d.save_diagram(new_diagram)
+    return {"status": "Diagram saved"}
